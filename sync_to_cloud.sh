382866a445857b230b81d7463905e4944d06fb25
JamesTran2407 <haith2407@gmail.com>
2025-04-21 23:25:58 +0700

    Delete sync_to_cloud.sh

--- sync_to_cloud.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")" || exit
-git pull origin main
-git add .
-git commit -m "🔄 Auto-sync from JennyCore at $(date '+%Y-%m-%d %H:%M:%S')" || echo "⚠️ Nothing new to commit."
-git push origin main
