71cd10fda1fd9162f6e9191027679cac9460f0f8
JamesTran2407 <haith2407@gmail.com>
2025-10-21 02:03:21 +0700

    Update fix-perl.sh

--- fix-perl.sh
+++ fix-perl.sh
@@ -1,11 +1,4 @@
-cat >/etc/apt/apt.conf.d/99no-translations <<'EOF'
-Acquire::Languages "none";
-EOF
-
-# 2) Dọn sạch index cũ
 rm -rf /var/lib/apt/lists/*
 mkdir -p /var/lib/apt/lists/partial
-apt clean
-
-# 3) Update lại (thêm retry + IPv4 cho chắc)
-apt-get -o Acquire::Retries=5 -o Acquire::ForceIPv4=true update
+echo 'Acquire::CompressionTypes::Order { "gz"; };' > /etc/apt/apt.conf.d/99gz.conf
+apt-get -o Acquire::CompressionTypes::Order::=gz update
